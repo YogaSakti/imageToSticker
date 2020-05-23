7a6ce2acecf2200473b8e1db75e88ee14bea8891
Yogs <yogazacty@gmail.com>
2020-05-24 02:50:41 +0700

    simple

---
+++ index.js
@@ -0,0 +1,87 @@
+const { create, decryptMedia } = require('@open-wa/wa-automate')
+const fs = require('fs-extra')
+const moment = require('moment')
+const mime = require('mime-types')
+
+const startServer = async (from) => {
+    create('Imperial', {
+            executablePath: 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe',
+            headless: false,
+            qrTimeout: 40,
+            authTimeout: 40,
+            autoRefresh: true,
+            qrRefreshS: 15,
+            restartOnCrash: startServer,
+            devtools: false,
+            chromiumArgs: [
+              '--no-sandbox',
+              '--disable-setuid-sandbox',
+              '--aggressive-cache-discard',
+              '--disable-cache',
+              '--disable-application-cache',
+              '--disable-offline-load-stale-cache',
+              '--disk-cache-size=0'
+            ]
+        })
+        .then(client => {
+            console.log('[SERVER] Server Started!')
+
+            // Force it to keep the current session
+            client.onStateChanged(state => {
+                console.log('[stateChanged]', state)
+                if (state === 'CONFLICT') client.forceRefocus()
+            })
+
+            client.onMessage((message) => {
+                msgHandler(client, message)
+            })
+        })
+}
+
+async function msgHandler (client, message) {
+    try {
+        // console.log(message)
+        const { type, body, from, t, sender, isGroupMsg, chat, caption, isMedia, mimetype } = message
+        const { id, pushname } = sender
+        const { name } = chat
+        const time = moment(t * 1000).format('DD/MM HH:mm:ss')
+        const commands = ['#sticker', '#halo']
+        const cmds = commands.map(x => x + '\\b').join('|')
+        const cmd = type === 'chat' ? body.match(new RegExp(cmds, 'gi')) : type === 'image' && caption ? caption.match(new RegExp(cmds, 'gi')) : ''
+
+        if (cmd) {
+            if (!isGroupMsg) console.log('[EXEC]', color(time, 'yellow'), color(cmd[0]), 'from', color(pushname))
+            if (isGroupMsg) console.log('[EXEC]', color(time, 'yellow'), color(cmd[0]), 'from', color(pushname), 'in', color(name))
+            const args = body.trim().split(' ')
+            switch (cmd[0]) {
+                case '#sticker':
+                    if (isMedia) {
+                        const mediaData = await decryptMedia(message)
+                        const imageBase64 = `data:${message.mimetype};base64,${mediaData.toString('base64')}`
+                        await client.sendImageAsSticker(from, imageBase64)
+                    } else {
+                        client.sendText(from, 'Tidak ada gambar! Untuk membuat sticker kirim gambar dengan caption #sticker ')
+                    }
+                    break
+                case '#halo':
+                        client.sendText(from, 'Hai')
+                    break
+            }
+        } else {
+          if (!isGroupMsg) console.log(color('[RECV]'), color(time, 'yellow'), 'Message from', color(pushname))
+            if (isGroupMsg) console.log(color('[RECV]'), color(time, 'yellow'), 'Message from', color(pushname), 'in', color(name))
+        }
+    } catch (err) {
+        console.log(color('[ERROR]', 'red'), err)
+    }
+}
+
+function color (text, color) {
+  switch (color) {
+    case 'red': return '\x1b[31m' + text + '\x1b[0m'
+    case 'yellow': return '\x1b[33m' + text + '\x1b[0m'
+    default: return '\x1b[32m' + text + '\x1b[0m' // default is green
+  }
+}
+
+startServer()
