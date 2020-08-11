9be554c84b45e4ec866fc2bead8a1a719ff7abbb
Yogs <yogazacty@gmail.com>
2020-08-11 19:24:48 +0700

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es6: true,
+    node: true
+  },
+  extends: 'standard',
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaVersion: 2018
+  },
+  rules: {
+    eqeqeq: 0,
+    indent: 0,
+    'no-unused-vars': 1,
+    'no-unused-expressions': 0,
+    'no-self-assign': 0,
+    'no-undef': 0,
+    'no-case-declarations': 0,
+    'prefer-promise-reject-errors': 1
+  }
+}
