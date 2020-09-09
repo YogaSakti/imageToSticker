ee9a278ad7afaa69298113ef3d12b5446ee45507
Yogs <yogazacty@gmail.com>
2020-09-09 16:56:35 +0700

    change some options

--- util/options.js
+++ util/options.js
@@ -9,7 +9,8 @@ module.exports = options = (headless) => {
     if (process.platform === 'win32' || process.platform === 'win64') {
         execPath = 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe'
     } else if (process.platform === 'linux') {
-        execPath = '/usr/bin/google-chrome-stable'
+        execPath = '/usr/bin/google-chrome-stable' 
+        // execPath = '/usr/bin/chromium-browser'
     } else if (process.platform === 'darwin') {
         execPath = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
     }
@@ -18,8 +19,7 @@ module.exports = options = (headless) => {
         headless: headless,
         executablePath: execPath,
         qrRefreshS: 10,
-        qrTimeout: 0,
-        authTimeout: 0,
+        qrTimeout: 10,
         autoRefresh: true,
         killProcessOnBrowserClose: true,
         cacheEnabled: false,
@@ -30,7 +30,7 @@ module.exports = options = (headless) => {
             '--disable-cache',
             '--disable-application-cache',
             '--disable-offline-load-stale-cache',
-            '--disk-cache-size=0',
+            '--disk-cache-size=0'
         ]
     }
 
