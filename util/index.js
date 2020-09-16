c3f6df9774a310a993c837f33164e383fc97e0df
Yogs <yogazacty@gmail.com>
2020-09-16 21:36:14 +0700

    fix fetcher and add something

--- util/index.js
+++
@@ -1,6 +0,0 @@
-exports.msgFilter = require('./msgFilter')
-exports.mentionList = require('./mention')
-exports.fetcher = require('./fetcher')
-exports.color = require('./color')
-exports.getBase64 = require('./getBase64')
-exports.options = require('./options')
