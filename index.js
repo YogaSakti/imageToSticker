5e3890b440869029c9639d6a1eb5c06dd702dc20
Yogs <yogazacty@gmail.com>
2020-09-16 21:39:29 +0700

    fix and add new feature

--- index.js
+++ index.js
@@ -1,52 +1,58 @@
-const { create } = require('@open-wa/wa-automate')
-const { color } = require('./util')
-const clientOptions = require('./util').options
+const { create, Client } = require('@open-wa/wa-automate')
+const { color } = require('./utils')
+const options = require('./utils/options')
 const msgHandler = require('./handler/message')
 
-const startServer = () => {
-    create('Imperial', clientOptions(true))
-        .then((client) => {
-            console.log('[DEV]', color('Red Emperor', 'yellow'))
-            console.log('[CLIENT] CLIENT Started!')
+const start = (client = new Client()) => {
+    console.log('[DEV]', color('Red Emperor', 'yellow'))
+    console.log('[CLIENT] CLIENT Started!')
 
-            // Force it to keep the current session
-            client.onStateChanged((state) => {
-                console.log('[Client State]', state)
-                if (state === 'CONFLICT') client.forceRefocus()
+    // Force it to keep the current session
+    client.onStateChanged((state) => {
+        console.log('[Client State]', state)
+        if (state === 'CONFLICT') client.forceRefocus()
+    })
+
+    // listening on message
+    client.onMessage((message) => {
+        client.getAmountOfLoadedMessages() // Cut message Cache if cache more than 3K
+            .then((msg) => {
+                if (msg >= 3000) {
+                    console.log('[CLIENT]', color(`Loaded Message Reach ${msg}, cuting message cache...`, 'yellow'))
+                    client.cutMsgCache()
+                }
             })
+        // Message Handler
+        msgHandler(client, message)
+    })
 
-            // listening on message
-            client.onMessage((message) => {
-                client.getAmountOfLoadedMessages() // Cut message Cache if cache more than 3K
-                    .then((msg) => {
-                        if (msg >= 3000) {
-                            console.log('[CLIENT]', color(`Loaded Message Reach ${msg}, cuting message cache...`, 'yellow'))
-                            client.cutMsgCache()
-                        }
-                    })
-                // Message Handler
-                msgHandler(client, message)
-            })
+    // listen group invitation
+    client.onAddedToGroup(({ groupMetadata: { id }, contact: { name } }) =>
+        client.getGroupMembersId(id)
+            .then((ids) => {
+                console.log('[CLIENT]', color(`Invited to Group. [ ${name} : ${ids.length}]`, 'yellow'))
+                // conditions if the group members are less than 10 then the bot will leave the group
+                if (ids.length <= 10) {
+                    client.sendText(id, 'Sorry, the minimum group member is 10 user to use this bot. Bye~').then(() => client.leaveGroup(id))
+                } else {
+                    client.sendText(id, `Hello group members *${name}*, thank you for inviting this bot, to see the bot menu send *#menu*`)
+                }
+            }))
 
-            // listen group invitation
-            client.onAddedToGroup(({ groupMetadata: { id }, contact: { name } }) =>
-                client.getGroupMembersId(id)
-                    .then((ids) => {
-                        console.log('[CLIENT]', color(`Invited to Group. [ ${name} : ${ids.length}]`, 'yellow'))
-                        // conditions if the group members are less than 10 then the bot will leave the group
-                        if (ids.length <= 10) {
-                            client.sendText(id, 'Sorry, the minimum group member is 10 user to use this bot. Bye~').then(() => client.leaveGroup(id))
-                        } else {
-                            client.sendText(id, `Hello group members *${name}*, thank you for inviting this bot, to see the bot menu send *#menu*`)
-                        }
-                    }))
+    client.onRemovedFromGroup((data) => {
+        // console.log(data)
+    })
 
-            // listen paricipant event on group (wellcome message)
-            // client.onGlobalParicipantsChanged((event) => {
-            //     if (event.action === 'add') client.sendTextWithMentions(event.chat, `Hello, Welcome to the group @${event.who.replace('@c.us', '')} \n\nHave fun with us✨`)
-            // })
-        })
-        .catch((err) => new Error(err))
+    // listen paricipant event on group (wellcome message)
+    client.onGlobalParicipantsChanged((event) => {
+        // if (event.action === 'add') client.sendTextWithMentions(event.chat, `Hello, Welcome to the group @${event.who.replace('@c.us', '')} \n\nHave fun with us✨`)
+    })
+
+    client.onIncomingCall((callData) => {
+        // client.contactBlock(callData.peerJid)
+    })
 }
 
-startServer()
+create('Imperial', options(true, start))
+    .then((client) => start(client))
+    .catch((err) => new Error(err))
